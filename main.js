01157d754d460693ae67311abaf050201539a321
Pokemom3 <92159837+Pokemom3@users.noreply.github.com>
2022-01-03 18:30:30 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+let buttonMoreQuotes = document.getElementById("button");
+buttonMoreQuotes.innerHTML = "Tell Me More";
+buttonMoreQuotes.type = "tellMeMore";
+buttonMoreQuotes.name = "moreQuotes";
+document.body.appendChild(buttonMoreQuotes);
